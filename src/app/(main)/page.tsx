fde16046ed0cff2b08fa5f4e54c9dea6e30a2c67
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-09-16 18:08:51 +0600

    updated the commit

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
       <motion.main
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 1 }} // -> Delay matches loader lifetime
+        transition={{ duration: 0.6, delay: 1 }} // Delay matches loader lifetime
       >
         {/* <main> */}
         {/* <Navbar /> */}
