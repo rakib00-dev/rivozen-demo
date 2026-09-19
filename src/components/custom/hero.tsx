03960108638efde50f1d78291e79f8fa4c51b675
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-09-19 19:46:43 +0600

    added a comment

--- src/components/custom/hero.tsx
+++ src/components/custom/hero.tsx
@@ -37,6 +37,7 @@ export function Hero() {
               <Star key={i} className="h-3.5 w-3.5 fill-primary text-primary" />
             ))}
           </span>
+          {/*  */}
           Rated 4.9/5 by 2,000+ teams
         </motion.div>
 
