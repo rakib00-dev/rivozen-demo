7dc68afde0e86c1ca668688c0f4425466a510176
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-09-25 19:58:55 +0600

    updated the website hero section

--- src/components/custom/services/website/PageWebsiteHero.tsx
+++ src/components/custom/services/website/PageWebsiteHero.tsx
@@ -3,7 +3,16 @@ import CommonHeroSection from "@/components/common/CommonHeroSection";
 export default function PageWebsiteHero() {
   return (
     <>
-      <CommonHeroSection />
+      <CommonHeroSection
+        smallHeading="Get in touch"
+        bigHeading={["Have a", "project"]}
+        bigHeadingItalic="in mind?"
+        paragraph="Tell us a little about what you're building, what you need, and where you want to go. We'll take it from there."
+        primaryBtn="Start a conversation"
+        primaryBtnLink="/contact"
+        secondaryBtn="Book A Call"
+        secondaryBtnLink="/book-a-call"
+      />
     </>
   );
 }
